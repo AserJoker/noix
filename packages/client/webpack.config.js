b2f0dbd1a72508dd2534f149b9ee6e14351c9399
aserjoker <aserjoker@outlook.com>
2021-03-21 15:50:17 +0800

    feat: add client package

---
+++ packages/client/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const webpack = require('webpack')
+const packageName = 'noix.client';
+module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: path.resolve(__dirname, 'src/index.ts'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/',
+    libraryTarget: 'commonjs',
+    filename: `${packageName}.lib.js`
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: ['ts-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts'],
+    alias: {
+      '@src': path.resolve(__dirname, 'src')
+    }
+  },
+};
