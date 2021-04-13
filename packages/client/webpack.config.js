e924175847f9ca7428470df9cdd19fb2ebc44e14
wjf <wjf@shushi.pro>
2021-04-13 19:15:08 +0800

    feat: fix api

--- packages/client/webpack.config.js
+++ packages/client/webpack.config.js
@@ -1,6 +1,4 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
 const packageName = 'noix.client';
 module.exports = {
   mode: 'development',
@@ -20,15 +18,5 @@ module.exports = {
   },
   resolve: {
     extensions: ['.ts', '.js']
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, './dist'),
-    port: 8001
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, './public/index.html')
-    }),
-    new webpack.ProvidePlugin({ $core: '@noix/core' })
-  ]
+  }
 };
