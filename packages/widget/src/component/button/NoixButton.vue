84a2f02c27a96e1c82f52b74c435d6fa063462d3
wjf <wjf@shushi.pro>
2021-04-22 18:40:57 +0800

    feat: fix some bug

--- packages/widget/src/component/button/NoixButton.vue
+++ packages/widget/src/component/button/NoixButton.vue
@@ -1,13 +1,16 @@
 <template>
-  <a-button @click="OnClick"><slot /></a-button>
+  <a-button @click="OnClick" :type="type"><slot /></a-button>
 </template>
 <script lang="ts">
-import { BaseWidget, Component, Emit } from '../../base';
+import { BaseWidget, Component, Emit, Prop } from '../../base';
 import { NoixBlock } from '../block';
 import { Button as AButton } from 'ant-design-vue';
 @Component({ components: { AButton } })
 export default class NoixButton extends BaseWidget {
   @Emit('click')
   private OnClick() {}
+
+  @Prop()
+  private type!: string;
 }
 </script>
--- packages/widget/src/index.ts
+++ packages/widget/src/index.ts
@@ -1,3 +1,4 @@
 import 'ant-design-vue/dist/antd.css';
+export * from './base';
 export * from './component';
 export * from './WidgetApplication';
